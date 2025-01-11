4fd14ae99356e27a975051a03805fbb67b36f8a4
abeaduhh <adiba.labonno@gmail.com>
2025-01-11 12:18:50 -0600

    function for presenting projects
    adding the functions

--- portfolio/src/pages/Projects.jsx
+++ portfolio/src/pages/Projects.jsx
@@ -1,27 +1,38 @@
 import React from "react";
+import {useState} from "react";
+
 
 export const Projects = () =>{
+
+    function inProgress(){
+        const [inProgress_projects, setInProgress] = useState([
+            {
+                id: 1, name: '2D Pixel Game'
+            },
+            {
+                id: 3, name: 'Identify Copy Move Forgery'
+            }
+        ]);
+    }
+    function completed(){
+        const [completed_projects, setCompleted] = useState([
+            {
+                id: 1, name: '2D Pixel Game'
+            },
+            {
+                id: 3, name: 'Identify Copy Move Forgery'
+            }
+        ]); 
+
+    }
+
     return(
         <div style={{textAlign: 'center'}} className= "Projectsdec" id="Projects">   
             <h1>Projects</h1>
                 <p>I've done a few projects in my free time, they are on my GitHub. Here are the ones I have completed so far:
                 </p>
-
-            <li>
-                <a href= "/Completed">Completed Projects</a>
-                <br></br>
                 <p>Here are some of the projects I am currently working on:</p>
-            </li>
-
-            <li>
-                <a href= "/WinnieThePooh">Winnie The Pooh Game</a>
                 <br></br>
-            </li>
-
-            <li>
-                <a href= "/Copy_Move">Identify Copy Move Forgery</a>
-                <br></br>
-            </li>
 
         </div> 
     )
